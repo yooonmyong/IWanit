96869357e71c3214d9f930944a7fde88814ee601
kym1229 <ymymym1229@gmail.com>
2021-07-17 03:16:21 +0900

    Add sign-in of client side

--- Client/Assets/Scripts/Account/SignInUser.cs
+++ Client/Assets/Scripts/Account/SignInUser.cs
@@ -1,10 +1,55 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Module;
 
 namespace Account
 {
     public class SignInUser : MonoBehaviour
     {
+        public InputField UserIDField;
+        public InputField UserPWDField;
+        public GameObject ErrorPopUpUI;
 
+        public void SignIn()
+        {
+            StartCoroutine(SignInCoroutine());
+        }
+
+        private IEnumerator SignInCoroutine()
+        {
+            var form = new WWWForm();
+            var URL = "http://127.0.0.1:/User/sign-in";
+            form.AddField("userID", UserIDField.text);
+            form.AddField("userPWD", UserPWDField.text);
+            UnityWebRequest www = UnityWebRequest.Post(URL, form);
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log($"{www.error}");
+            }
+            else
+            {
+                Debug.Log("Success to send name data to server!");
+            }
+
+            if (www.responseCode == 403)
+            {
+                ErrorPopUpUI.SetActive(true);
+            }
+            else
+            {
+                var response = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+                var result = JsonConvert.DeserializeObject<JObject>(response);
+                Debug.Log("Success to sign in");
+            }
+        }
     }
 }
