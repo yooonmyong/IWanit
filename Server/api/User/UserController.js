6e475dc95b271ca0292ef30831ef42126c35265b
kym1229 <ymymym1229@gmail.com>
2021-08-03 15:02:27 +0900

    Add logic about finding password (send temporary password)

--- Server/api/User/UserController.js
+++ Server/api/User/UserController.js
@@ -1,5 +1,7 @@
 const User = require('../../models')['user'];
+const sequelize = require('../../models/index');
 const UserService = require('../../services/UserService');
+const config = require('../../config/config.json');
 
 module.exports = {
     SignUp: async (req, res) => {
@@ -37,8 +39,56 @@ module.exports = {
     UpdateInfo: (req, res, next) => {
     },
     FindID: (req, res, next) => {
+    FindPWD: async (req, res) => {
+        const userID = req.body.userID;
+        const userEmail = req.body.userEmail;
+        const temporaryPWD = Math.random().toString(36).slice(2);
+
+        sequelize['user_db'].transaction(async (tx) => {
+            const user = await User
+                .findOne({
+                    where: {
+                        ID: userID,
+                        email: userEmail
+                    }
+                }, { transaction: tx });
+
+            if (!user) {
+                return res.status(404).send({ "message": "Not exist user" });
+            }
+
+            await new Promise(async (resolve, reject) => {
+                var result = await UserService.HashPWD(temporaryPWD);
+
+                if (result.hashedPWD) {
+                    User
+                        .update({ PWD: result.hashedPWD }, {
+                            where: {
+                                ID: user.ID,
+                                email: user.email
+                            }
+                        }, { transaction: tx })
+                        .then(() => {
+                            console.log('Send temporary password to valid email');
+                            req.session.sender = config.development.nodemailer.senderID;
+                            req.session.receiver = userEmail;
+                            req.session.subject = '[IWanit] 회원님의 임시 비밀번호 입니다'
+                            req.session.content = temporaryPWD + ' 입니다. 로그인 후 꼭 비밀번호를 변경해주시기 바랍니다.';
+
+                            return res.redirect("/User/SendEmail");
+                        })
+                        .catch((sequelizeError) => {
+                            console.log(sequelizeError);
+                            return res.status(500).send({ "Sequelize module occured error": sequelizeError });
+                        });
+                    resolve();
+                }
+                else {
+                    reject(res.status(500).send({ "message": result.message }));
+                }
+            });
+        });
     },
-    FindPWD: (req, res, next) => {
     },
     DeleteUser: (req, res, next) => {
     }
