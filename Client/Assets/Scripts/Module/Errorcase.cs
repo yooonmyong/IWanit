59ea52a94381464172f3395e975693038e96e0a5
kym1229 <ymymym1229@gmail.com>
2021-08-15 10:50:49 +0900

    #2

--- Client/Assets/Scripts/Module/Errorcase.cs
+++ Client/Assets/Scripts/Module/Errorcase.cs
@@ -1,10 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Module
 {
-    public enum Errorcase 
-    { 
-        [StringValue("Inappropriate ID")] Error1, 
-        [StringValue("Inappropriate PWD")] Error2,
-        [StringValue("Not matched PWD")] Error3,
-        [StringValue("Duplicated ID")] Error4
-    }    
+    public class Errorcase
+    {
+        public Dictionary<string, string> Errorcases;
+
+        public Errorcase()
+        {
+            Errorcases = new Dictionary<string, string>();
+            Errorcases.Add
+            (
+                "Inappropriate ID", "아이디 조건을 만족하지 않습니다."
+            );
+            Errorcases.Add
+            (
+                "Inappropriate PWD", "비밀번호 조건을 만족하지 않습니다."
+            );
+            Errorcases.Add
+            (
+                "Not matched PWD", "비밀번호 간에 일치하지 않습니다."
+            );
+            Errorcases.Add
+            (
+                "Duplicated ID", "이미 사용 중인 아이디 혹은 이메일 입니다."
+            );
+        }
+
+        public string GetErrorcase(string errorcase)
+        {
+            return Errorcases[errorcase];
+        }
+    }
 }
