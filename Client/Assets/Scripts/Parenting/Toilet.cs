3b74e4b7e8d217cf8ccea545270ec599183fc503
kym1229 <ymymym1229@gmail.com>
2022-01-05 22:20:06 +0900

    Add logic to count cooldown time

--- Client/Assets/Scripts/Parenting/Toilet.cs
+++ Client/Assets/Scripts/Parenting/Toilet.cs
@@ -1,22 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UI;
 
 namespace Parenting
 {
     public class Toilet : MonoBehaviour
     {
-        private void Awake()
+        public Toast toastPopup;
+        private bool isAvailable;
+        private float timer;
+        private float fullTime;
+
+        private void Start()
         {
+            isAvailable = true;
+            timer = 0.0f;
+            fullTime = 300.0f;
+            StartCoroutine(SetMonthsCoroutine());
         }
         
         private void Update()
         {
+            timer += Time.deltaTime;
+            if (timer == fullTime)
+            {
+                timer = 0.0f;
+                isAvailable = true;
+            }
         }
 
         private void OnMouseUpAsButton()
         {
             Debug.Log("toilet is clicked");
+            CheckAvailable();
+        }
+
+        private void CheckAvailable()
+        {
+            if (!isAvailable)
+            {
+                Debug.Log("Baby has just peed!");
+                toastPopup.Appear();
+            }
+            else
+            {
+                isAvailable = false;
+                Pee();
+            }
+        }
         }
     }
 }
