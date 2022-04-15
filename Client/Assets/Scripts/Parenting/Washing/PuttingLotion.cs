b6f9d2cb42f458ed2d0088dfb44ea30b5afe0438
kym1229 <ymymym1229@gmail.com>
2022-04-15 19:56:33 +0900

    Add putting lotion

---
+++ Client/Assets/Scripts/Parenting/Washing/PuttingLotion.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Module;
+
+namespace Parenting
+{
+    public class PuttingLotion : MonoBehaviour, IDragHandler
+    {
+        public bool isDone;
+        public BoxCollider2D[] cleanEffectColliders;
+        public GameObject cleanEffectPrefab;
+        public Spawning spawning;
+        private Animator lotionAnimator;
+        private int countingPuttingLotion;
+        private List<GameObject> cleanEffects;
+        private Vector2 originalTransform;
+        private RectTransform rectTransform;
+
+        private void Start()
+        {
+            this.GetComponent<Button>().enabled = false;
+            isDone = false;
+            rectTransform = this.GetComponent<RectTransform>();
+            originalTransform = rectTransform.anchoredPosition;
+            rectTransform.anchoredPosition = Constants.CenterToolTransform;
+            lotionAnimator = this.GetComponent<Animator>();
+            countingPuttingLotion = 0;
+            cleanEffects = new List<GameObject>();
+        }
+
+        private void Update()
+        {
+            if (cleanEffects.Count >= Constants.CleanEnough)
+            {
+                foreach (var cleanEffect in cleanEffects)
+                {
+                    Destroy(cleanEffect);
+                }
+
+                rectTransform.anchoredPosition = originalTransform;
+                isDone = true;
+            }
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            rectTransform.anchoredPosition += eventData.delta;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.name.Equals("Body"))
+            {
+                lotionAnimator.SetTrigger("pump");
+                countingPuttingLotion++;
+                var index = 
+                    countingPuttingLotion % Constants.triggerCleanEffect;
+
+                if (countingPuttingLotion > 0 && index == 0)
+                {
+                    var settingLeftorRight = 
+                        (countingPuttingLotion / Constants.triggerCleanEffect) 
+                        % 2;
+
+                    MakeCleanEffect(settingLeftorRight);
+                }
+            }
+        }
+
+        private void MakeCleanEffect(int leftOrRight)
+        {
+            // WIP
+            var cleanEffect =
+                spawning.SpawnRandomPosition
+                (
+                    cleanEffectPrefab, cleanEffectColliders[leftOrRight], 1.0f
+                );
+            
+            cleanEffects.Add(cleanEffect);
+        }
+    }
+}
